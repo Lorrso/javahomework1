03d7dec8d8593a172d9fa779698be40b702c5ed0
AnnaShev <anreich0@gmail.com>
2023-07-31 13:45:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 15_000;
+        int miles = (ticket / 20);
+
+        System.out.println((miles) + " бонусных миль начислено за билет стоимостью " + (ticket) + " рублей");
+
+    }
+}
